dd36f298fb0c5e85d2e408f63dfc280d1f1b92d9
Daniel Rexin <daniel@medinion.de>
2026-02-19 12:23:24 +0100

    Corrects version

--- src/xmldict/__version__.py
+++ src/xmldict/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "XmlDict"
 __description__ = "XML JSON conversion"
 __url__ = ""
-__version__ = "1.1.1"
+__version__ = "1.1.3"
 __author__ = "Daniel Rexin"
 __author_email__ = "daniel@medinion.de"
 __license__ = "MIT"
